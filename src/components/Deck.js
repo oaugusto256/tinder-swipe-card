1f14c21ccb31baf3ffa81d98242a8a7c207d197f
Otavio Augusto <tavioalves@gmail.com>
2019-05-11 19:24:55 -0300

    feat: Add PanResponder to the Deck component

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import { View, Animated, PanResponder } from 'react-native';
 
 export default class Deck extends Component {
+  constructor(props) {
+    super(props);
+
+    const panResponder = PanResponder.create({});
+
+    this.panResponder = panResponder;
+  }
+
   renderCards() {
     return this.props.data.map(card => {
       return this.props.renderCard(card);
